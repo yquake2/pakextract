98a66c68cdfec3cfde73ae5a414161cb44871d92
Daniel Gibson <metalcaedes@gmail.com>
2016-04-02 00:19:11 +0200

    Update Copyright

--- pakextract.c
+++ pakextract.c
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2012 Yamagi Burmeister
+ * Copyright (c) 2012-2016 Yamagi Burmeister
+ *               2015-2016 Daniel Gibson
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
